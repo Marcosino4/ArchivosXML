ba774ec0eeb546ef10f1fa8fb2f8a2a1aadf8550
Marcosino4 <marcosino654@gmail.com>
2023-11-14 13:40:31 +0100

    Falta crear objeto

---
+++ src/lectura/LeerXML.java
@@ -0,0 +1,58 @@
+package lectura;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+public class LeerXML {
+
+	public static void main(String[] args) {
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newDefaultInstance();
+		DocumentBuilder db;
+		Document document;
+		File file;
+		XPath xPath;
+		NodeList nodeList;
+		String path;
+		
+		
+		try {
+			db = dbf.newDocumentBuilder();
+			file = new File("personas.xml");
+			document = db.parse(file);
+			
+			document.normalizeDocument();
+			
+			xPath = XPathFactory.newInstance().newXPath();
+			
+			//consulta
+			path = "//persona";
+			
+			//nodos resultantes de la consulta
+			nodeList = (NodeList) xPath.compile(path).evaluate(document, XPathConstants.NODESET);
+			
+			for(int i = 0; i < nodeList.getLength(); i++) {
+				Element elemento = (Element) nodeList.item(i);
+				
+				System.out.println(elemento.getElementsByTagName("nombre").item(0).getTextContent());
+				System.out.println(elemento.getElementsByTagName("apellido").item(0).getTextContent());
+				System.out.println(elemento.getElementsByTagName("nacimiento").item(0).getTextContent());
+				System.out.println("----------");
+
+
+
+			}
+		}catch (Exception e) {
+			System.out.println(e.getMessage());		
+		}
+	}
+
+}
